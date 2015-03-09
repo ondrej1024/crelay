35ca332898b1d5e39def2408ade0a145ab36d900
Ondrej Wisniewski <ondrej.wisniewski@dektech.com.au>
2015-03-09 12:14:08 +0100

    Added source files for sample driver

--- src/relay_drv_sample.h
+++ src/relay_drv_sample.h
@@ -1,22 +1,38 @@
 /******************************************************************************
  * 
- * Relay card control utility: Driver for XYZ relay card
- *   http:...
+ * Relay card control utility: Driver for <XYZ> relay card
+ *   <Link to relay cards product web page>
  * 
  * Description:
+ *   <Short description of what is implemented in this file>
  * 
- * Author:
- *
  * Build instructions:
  *   gcc -c relay_drv_sample.c
  * 
  * Last modified:
- *   
+ *   <date>
+ * 
+ * Copyright 2015, <your name>
+ * 
+ * This file is part of crelay.
+ * 
+ * crelay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with crelay.  If not, see <http://www.gnu.org/licenses/>.
  *
  *****************************************************************************/ 
 
-#ifndef relay_drv_conrad_h
-#define relay_drv_conrad_h
+#ifndef relay_drv_sample_h
+#define relay_drv_sample_h
 
 /**********************************************************
  * Function detect_com_port_sample()
